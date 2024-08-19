5cbd99047dc46dce37b27322c9dd62eab0db4cd5
httpe <httpe@live.jp>
2024-08-18 20:50:32 -0400

    Add proxy

--- server.py
+++ server.py
@@ -7,13 +7,14 @@ from urllib.parse import urlunparse, urlencode
 from collections import namedtuple
 from contextlib import asynccontextmanager
 from abc import ABC, abstractmethod
+from typing_extensions import Self
 
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, PositiveInt, model_validator
 from enum import Enum
 
 from uvicorn.config import LOGGING_CONFIG
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, Response
 import uvicorn
 
 import httpx
@@ -33,7 +34,12 @@ async def lifespan(app: FastAPI):
     global plant, server, network
     # This cannot be moved into __main__ as FastAPI will load the script without __main__ for sub-workers
     plant, server = get_server_settings()
-    network = HttpNetworkInterface()
+    # Use proxy if set so
+    if plant.use_proxy and plant.proxy is not None and server != plant.proxy:
+        logger.warning(f"Using proxy: {plant.proxy.address}")
+        network = HttpNetworkInterfaceWithProxy(proxy_addr=plant.proxy.address)
+    else:
+        network = HttpNetworkInterface()
     yield
     # TODO: cleanup
     pass
@@ -49,10 +55,10 @@ logger = logging.getLogger("uvicorn.error")
 ############################################
 
 
-class NetworkAddress:
-    def __init__(self, host: str | None, port: int) -> None:
-        self.host = host
-        self.port = port
+class NetworkAddress(BaseModel):
+    name: str
+    host: str | None
+    port: PositiveInt
 
     def construct_base_url(self, path: str):
         if self.host is None:
@@ -65,7 +71,7 @@ class NetworkAddress:
 
 class NetworkInterface(ABC):
     @abstractmethod
-    def send(
+    def call(
         self, destination: NetworkAddress, endpoint: str, params: dict | None = None
     ) -> str:
         pass
@@ -95,12 +101,40 @@ class HttpNetworkInterface(NetworkInterface):
     def __init__(self) -> None:
         super().__init__()
 
-    def send(
+    def call(
         self, destination: NetworkAddress, endpoint: str, params: dict | None = None
     ) -> str:
         base_url = destination.construct_base_url(endpoint)
-        logger.info(f"Getting url: {base_url} with params {params}")
+        logger.info(f"Calling {base_url} with params {params}")
         r = httpx.get(base_url, params=params)
+        return r.text
+
+
+class NetworkRequest(BaseModel):
+    origin: NetworkAddress
+    destination: NetworkAddress
+    endpoint: str
+    params: dict | None
+
+
+class HttpNetworkInterfaceWithProxy(HttpNetworkInterface):
+    def __init__(self, proxy_addr: NetworkAddress) -> None:
+        super().__init__()
+        self.proxy_addr = proxy_addr
+
+    def call(
+        self, destination: NetworkAddress, endpoint: str, params: dict | None = None
+    ) -> str:
+        base_proxy_url = self.proxy_addr.construct_base_url("/route")
+        request = NetworkRequest(
+            origin=server.address,
+            destination=destination,
+            endpoint=endpoint,
+            params=params,
+        )
+        data = request.model_dump()
+        logger.info(f"Sending request via proxy {self.proxy_addr.name}: {data}")
+        r = httpx.post(base_proxy_url, json=data)
         return r.text
 
 
@@ -115,6 +149,19 @@ async def get_root():
     return RedirectResponse("/docs")
 
 
+@app.post("/route")
+async def proxy_route(request_to_forward: NetworkRequest) -> Response:
+    logger.info(
+        f"Routing request from {request_to_forward.origin.name} to {request_to_forward.destination.name} for endpoint {request_to_forward.endpoint} with params {request_to_forward.params}"
+    )
+    r = network.call(
+        destination=request_to_forward.destination,
+        endpoint=request_to_forward.endpoint,
+        params=request_to_forward.params,
+    )
+    return Response(content=r, media_type="application/json")
+
+
 class PongResponse(BaseModel):
     server_name: str
     server_id: str
@@ -123,6 +170,7 @@ class PongResponse(BaseModel):
 
 @app.get("/pong")
 async def pong(requester: str | None = None) -> PongResponse:
+    logger.info(f"Pong requested by {requester}")
     resp = PongResponse(
         server_name=server.name,
         server_id=server.id,
@@ -139,8 +187,10 @@ class PingResponse(BaseModel):
 
 @app.get("/ping/{server_name}")
 async def ping(server_name: str) -> PingResponse:
+    logger.info(f"Ping-ing {server_name}")
     destination = plant.get_server(server_name).address
-    result_json = network.send(destination, "/pong", {"requester": server.name})
+    result_json = network.call(destination, "/pong", {"requester": server.name})
+    logger.info(f"Pong respond received from {destination.name}")
     remote_resp = PongResponse.model_validate_json(result_json)
     resp = PingResponse(
         server_name=server.name,
@@ -190,14 +240,25 @@ class ServerConfig(BaseModel):
 
     @property
     def address(self):
-        return NetworkAddress(self.host, self.port)
+        return NetworkAddress(name=self.name, host=self.host, port=self.port)
 
 
 class PlantConfig(BaseModel):
     servers: list[ServerConfig]
+    use_proxy: bool
+    proxy: ServerConfig | None
 
     def get_server(self, name: str):
+        if self.proxy is not None:
+            if self.proxy.name == name:
+                return self.proxy
         return next(x for x in self.servers if x.name == name)
+
+    @model_validator(mode="after")
+    def check_proxy(self) -> Self:
+        if self.use_proxy and self.proxy is None:
+            raise ValueError('if "use_proxy" is true, "proxy" must be defined')
+        return self
 
 
 def parse_cml_args():
@@ -235,7 +296,7 @@ def main():
         port=server.port,
         log_level=server.log_level.value,
         log_config=log_config,
-        reload=False,  # reload=True seems to block Ctrl+C exit on Windows
+        reload=True,
     )
 
 

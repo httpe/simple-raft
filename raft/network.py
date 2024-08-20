64ee619ab9e2403ce8d13f93aca57219096796ec
httpe <httpe@live.jp>
2024-08-19 21:51:26 -0400

    Allow proxy to drop/timeout requests

--- raft/network.py
+++ raft/network.py
@@ -1,11 +1,11 @@
 from urllib.parse import urlunparse, urlencode
 from collections import namedtuple
-
+from asyncio import sleep
 from abc import ABC, abstractmethod
 
 import httpx
 from pydantic import BaseModel, PositiveInt
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, HTTPException, status
 
 from .logger import logger
 from .singleton import singleton
@@ -15,6 +15,24 @@ from .singleton import singleton
 ############################################
 
 router = APIRouter()
+
+
+############################################
+## Network Exceptions
+############################################
+
+
+class NetworkException(Exception):
+    pass
+
+
+class NetworkTimeoutException(NetworkException):
+    pass
+
+
+class NetworkGeneralException(NetworkException):
+    pass
+
 
 ############################################
 ## Network Interface
@@ -82,11 +100,13 @@ class HttpNetworkInterface(NetworkInterface):
         logger.info(f"Calling {base_url} with params {params}")
         try:
             r = httpx.get(base_url, params=params, timeout=singleton.server.timeout)
-            return r.text
         except httpx.TimeoutException as e:
             raise NetworkTimeoutException()
         except Exception as e:
             raise NetworkGeneralException() from e
+        if r.status_code != status.HTTP_200_OK:
+            raise NetworkGeneralException(r.status_code)
+        return r.text
 
 
 ############################################
@@ -113,11 +133,13 @@ class HttpNetworkInterfaceWithProxy(HttpNetworkInterface):
         logger.info(f"Sending request via proxy {self.proxy_addr.name}: {data}")
         try:
             r = httpx.post(base_proxy_url, json=data)
-            return r.text
         except httpx.TimeoutException as e:
             raise NetworkTimeoutException() from e
         except Exception as e:
             raise NetworkGeneralException() from e
+        if r.status_code != status.HTTP_200_OK:
+            raise NetworkGeneralException(r.status_code)
+        return r.text
 
 
 @router.post("/route")
@@ -125,9 +147,37 @@ async def proxy_route(request_to_forward: NetworkRequest) -> Response:
     logger.info(
         f"Routing request from {request_to_forward.origin.name} to {request_to_forward.destination.name} for endpoint {request_to_forward.endpoint} with params {request_to_forward.params}"
     )
+
+    process_proxy_drop_rule(request_to_forward)
+    await process_proxy_timeout_rule(request_to_forward)
+
     r = singleton.network.call(
         destination=request_to_forward.destination,
         endpoint=request_to_forward.endpoint,
         params=request_to_forward.params,
     )
+
     return Response(content=r, media_type="application/json")
+
+
+def process_proxy_drop_rule(request: NetworkRequest):
+    # if (
+    #     request.origin.name == "B"
+    #     and request.destination.name == "A"
+    #     and request.endpoint == "/pong"
+    # ):
+    #     raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY)
+    pass
+
+
+INFINITE_SLEEP_PERIOD = 60 * 60 * 24 * 30
+
+
+async def process_proxy_timeout_rule(request: NetworkRequest):
+    # if (
+    #     request.origin.name == "B"
+    #     and request.destination.name == "A"
+    #     and request.endpoint == "/pong"
+    # ):
+    #     await sleep(INFINITE_SLEEP_PERIOD)
+    pass

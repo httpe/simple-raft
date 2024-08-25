bcd6bcec7d4ae8795188b2f4c38c60b918ec3937
httpe <httpe@live.jp>
2024-08-25 19:05:28 -0400

    Abstracted testing cases

--- raft/client.py
+++ raft/client.py
@@ -1,13 +1,17 @@
 from typing import TypeVar, Type
+from abc import ABC, abstractmethod
+import random
+import string
 import argparse
 import json
 import logging
+import time
+
 import httpx
 from fastapi import status
 from pydantic import BaseModel
 
 from .configs import PlantConfig, ServerConfig
-from .network import NetworkGeneralException
 from .api import (
     TPC_GET_ENDPOINT,
     DBGetArg,
@@ -24,10 +28,18 @@ from .api import (
     ProxyClearRulesArg,
 )
 
+#############################################
+## Logging
+#############################################
+
 logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 TResp = TypeVar("TResp", bound=BaseModel)
+
+#############################################
+## Util
+#############################################
 
 
 def call_api(
@@ -36,31 +48,149 @@ def call_api(
     url = server.address.construct_base_url(endpoint)
     r = httpx.post(url, json=arg.model_dump())
     if r.status_code != status.HTTP_200_OK:
-        raise NetworkGeneralException(r.status_code)
+        raise Exception(r.status_code)
     return resp_class(**r.json())
 
 
-def test_tpc_simple_sync(main: ServerConfig, sub: ServerConfig):
-    logger.info(f"Two-Phase-Commit: Running simple sync test between {main} and {sub}")
-    key = "abc"
-    data = "xyz"
-    write = call_api(
-        main, TPC_SET_ENDPOINT, DBSetArg(key=key, data=data), DBSetResponse
+def gen_random_str(N: int):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=N))
+
+
+#############################################
+## Abstract Test Cases
+#############################################
+
+
+class DBInterface(ABC):
+    @abstractmethod
+    def read(self, server: ServerConfig, key: str) -> str | None:
+        pass
+
+    @abstractmethod
+    def write(self, server: ServerConfig, key: str, data: str | None):
+        pass
+
+
+def test_two_server_sync(main: ServerConfig, sub: ServerConfig, db: DBInterface):
+    logger.info(f"Test started: simple sync between {main} and {sub}")
+    key = "test_two_server_sync"
+    # Write data
+    data = gen_random_str(10)
+    logger.info(f"Writing data, key: {key}, data: {data}")
+    db.write(main, key, data)
+    assert db.read(sub, key) == data
+    assert db.read(main, key) == data
+    # Update data
+    data_alt = gen_random_str(10)
+    logger.info(f"Updating data, key: {key}, data: {data_alt}")
+    db.write(sub, key, data_alt)
+    assert db.read(sub, key) == data_alt
+    assert db.read(main, key) == data_alt
+    # Remove data
+    logger.info(f"Removing data, key: {key}")
+    db.write(sub, key, None)
+    assert db.read(main, key) is None
+    assert db.read(sub, key) is None
+    logger.info(f"Test completed: simple sync between {main} and {sub}")
+
+
+def test_eventual_consistency_after_network_partition(
+    proxy: ServerConfig,
+    main: ServerConfig,
+    sub: ServerConfig,
+    db: DBInterface,
+    timeout_sec=10,
+):
+    logger.info(
+        f"Test started: Eventual consistency after network partition between {main} and {sub}"
     )
-    logger.info(f"TPC SET response: {write}")
-    assert write.key == key
-    read_sub = call_api(sub, TPC_GET_ENDPOINT, DBGetArg(key=key), DBGetResponse)
-    read_main = call_api(main, TPC_GET_ENDPOINT, DBGetArg(key=key), DBGetResponse)
-    logger.info(f"TPC GET main response: {read_main}")
-    logger.info(f"TPC SET sub response: {read_sub}")
-    assert read_sub.entry is not None and read_sub.entry.data == data
-    assert read_main.entry is not None and read_main.entry.data == data
+
+    # Make proxy to drop all packets, i.e. introduce network partition
+    logger.info(f"Introducing network partition")
+    criteria = RequestMatchingCriteria(
+        origin_names=None, dest_names=None, endpoints=None
+    )
+    call_api(
+        proxy,
+        PROXY_RULE_SET_ENDPOINT,
+        ProxySetRuleArg(rule="drop", id="drop_all", criteria=criteria),
+        ProxySetRuleResponse,
+    )
+
+    key = "test_eventual_consistency_after_network_partition"
+
+    # Write data
+    data = gen_random_str(10)
+    logger.info(f"Writing data during network partition, key: {key}, data: {data}")
+    # We expect the data write and read on the main node to be successful
+    db.write(main, key, data)
+    assert db.read(main, key) == data
+    # sub node should work but doesn't need to be consistent
+    sub_data = db.read(sub, key)
+
+    # Resume network
+    logger.info(f"Resuming network")
+    call_api(
+        proxy,
+        PROXY_CLEAR_RULES_ENDPOINT,
+        ProxyClearRulesArg(rule="drop", ids=["drop_all"]),
+        ProxyClearRulesResponse,
+    )
+
+    # Wait for eventual consistency to realize until timeout
+    logger.info(f"Waiting for eventual consistency to realize")
+    t0 = time.time()
+    while time.time() - t0 < timeout_sec:
+        sub_data_next = db.read(sub, key)
+        if sub_data_next != sub_data:
+            assert sub_data_next == data
+            logger.info(f"Sub node state is synced now")
+            break
+        else:
+            assert sub_data_next == sub_data
+            logger.info(f"Sub node state not synced yet, will retry in 0.5s...")
+        time.sleep(0.5)
+
+    logger.info(
+        f"Test completed: Eventual consistency after network partition between {main} and {sub}"
+    )
 
 
-def test_tpc(main: ServerConfig, sub: ServerConfig):
-    logger.info(f"Start testing Two-Phase-Commit between {main} and {sub}")
-    test_tpc_simple_sync(main, sub)
-    logger.info(f"Two-Phase-Commit tests finished")
+def test_all(plant: PlantConfig, db: DBInterface):
+    logger.info(f"Start running all tests")
+
+    # Normal test
+    test_two_server_sync(plant.servers[0], plant.servers[1], db)
+
+    # Network partition test
+    assert plant.proxy is not None
+    logger.info(f"Using proxy {plant.proxy}")
+    test_eventual_consistency_after_network_partition(
+        plant.proxy, plant.servers[0], plant.servers[1], db
+    )
+
+    logger.info(f"All tests finished")
+
+
+#############################################
+## Two Phase Commit
+#############################################
+
+
+class TPC(DBInterface):
+    def read(self, server: ServerConfig, key: str) -> str | None:
+        r = call_api(server, TPC_GET_ENDPOINT, DBGetArg(key=key), DBGetResponse)
+        if r.entry is None:
+            return None
+        return r.entry.data
+
+    def write(self, server: ServerConfig, key: str, data: str | None):
+        call_api(server, TPC_SET_ENDPOINT, DBSetArg(key=key, data=data), DBSetResponse)
+
+
+#############################################
+## Main
+#############################################
 
 
 def main():
@@ -71,40 +201,10 @@ def main():
     plant = PlantConfig(**config)
     assert plant.proxy is not None
 
-    main = plant.servers[0]
-    sub = plant.servers[1]
+    # Test two phase commit
+    db = TPC()
 
-    logger.info(f"Start running tests")
-
-    test_tpc(main, sub)
-
-    criteria = RequestMatchingCriteria(
-        origin_names=None, dest_names=None, endpoints=None
-    )
-    call_api(
-        plant.proxy,
-        PROXY_RULE_SET_ENDPOINT,
-        ProxySetRuleArg(rule="drop", id="drop_all", criteria=criteria),
-        ProxySetRuleResponse,
-    )
-
-    try:
-        test_tpc(main, sub)
-        assert False
-    except NetworkGeneralException as e:
-        logger.info(f"Expected error: {e}")
-        pass
-
-    call_api(
-        plant.proxy,
-        PROXY_CLEAR_RULES_ENDPOINT,
-        ProxyClearRulesArg(rule="drop", ids=["drop_all"]),
-        ProxyClearRulesResponse,
-    )
-
-    test_tpc(main, sub)
-
-    logger.info(f"Tests finished running")
+    test_all(plant, db)
 
 
 def parse_cml_args():
